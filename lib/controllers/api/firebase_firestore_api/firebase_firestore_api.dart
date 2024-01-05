32145c01260d638a4d1d59d34c6e40ebfa82c46d
priyanshuuuyadav <priyanshuuuyadav@gmail.com>
2024-01-05 21:18:37 +0530

    Update firebase_firestore_api.dart
    i removed a space from firebase_firestore_api file.
    No need to panic

--- lib/controllers/api/firebase_firestore_api/firebase_firestore_api.dart
+++ lib/controllers/api/firebase_firestore_api/firebase_firestore_api.dart
@@ -11,6 +11,6 @@ class AppFirebaseFirestoreApi {
   Future<void> delete({required String docId, required Map<String, dynamic> data}) async => await db.collection(collection).doc(docId).delete();
   Future<DocumentSnapshot<Map<String, dynamic>>> getOne({required String docId}) async => await db.collection(collection).doc(docId).get();
   Future<QuerySnapshot<Map<String, dynamic>>> getAll({required String docId}) async => await db.collection(collection).get();
-   Stream<DocumentSnapshot<Map<String, dynamic>>> getOneAsStream({required String docId})  =>  db.collection(collection).doc(docId).snapshots();
+  Stream<DocumentSnapshot<Map<String, dynamic>>> getOneAsStream({required String docId})  =>  db.collection(collection).doc(docId).snapshots();
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllAsStream({required String docId})  =>  db.collection(collection).snapshots();
 }
